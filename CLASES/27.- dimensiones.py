c3a1894efde65ff5e6eba9ba8b7c60939de11d26
Catalinafloresrojo <103601088+Catalinafloresrojo@users.noreply.github.com>
2022-06-29 16:41:17 -0400

    clases

--- CLASES/27.- dimensiones.py
+++ CLASES/27.- dimensiones.py
@@ -6,6 +6,24 @@ def main():
     #escalar
     escalar = np.array(42)
     print (escalar)
+    print(escalar.ndim) # .ndim dira la dimension
 
-    if __name__ == '__main__':
-        pass
+    #vector
+    vector = np.array([1, 2, 3])
+    print(vector)
+    print(vector.ndim)
+
+    #matriz
+    matriz = np.array([[1, 2, 3], [4, 5, 6]])
+    print(matriz)
+    print(matriz.ndim)
+
+    #tensor
+    tensor = np.array([[[1, 2, 3], [4, 5, 6]] , [[7, 8, 9] , [10, 11, 12]]])
+    print(tensor)
+    print(tensor.ndim)
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ CLASES/28.- Ejercicio 2.py
@@ -0,0 +1,2 @@
+# escribir un programa que lea un archivo del ejercicio 1 y agregue cada linea 
+# en una lista y luego imprima la lista. 
